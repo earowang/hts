996ff67d997edf758be024ebb7a702a9d187ccda
Earo Wang <earo.wang@gmail.com>
2013-12-15 23:58:58 +1100

    correct jaslot for gts

--- R/smatrix.R
+++ R/smatrix.R
@@ -5,11 +5,12 @@ smatrix <- function(xts) {
   #   xts: hts/gts
   #
   # Returns:
-  #   S matrix in the sparse form
+  #   S matrix in the sparse mode
   library(SparseM)
   if (is.hts(xts)) {
     gmat <- GmatrixH(xts$nodes)
     ntotal <- sum(Mnodes(xts$nodes))
+    Jaslot <- function(x) as.integer(rep(x[nrow(x), ], nrow(x)))
   } else {
     gmat <- xts$groups
     ntotal <- sum(Mlevel(xts$groups))
@@ -19,20 +20,21 @@ smatrix <- function(xts) {
   ra <- as.numeric(rep(1, nbts * nrow(gmat)))
 
   # Create Iaslot
-  Iaslot <- function(x) as.integer(cumsum(c(1L, unlist(apply(x, 1, table)))))
+  Iaslot <- function(xmat) as.integer(cumsum(c(1L, 
+                                      unlist(apply(xmat, 1, table)))))
   # Create Jaslot
-  Jaslot <- function(x) {
-    ja <- vector(length = nrow(x), mode = "list")
-    uni.num <- apply(x, 1, unique)
+  Jaslot <- function(xmat) {
+    ja <- vector(length = nrow(xmat), mode = "list")
+    uni.num <- apply(xmat, 1, unique)
+    if (is.matrix(uni.num)) {
+      uni.num <- split(uni.num, rep(1L:ncol(uni.sum), each = nrow(uni.num)))
+    }
     for (i in 1L:length(ja)) {
-      ja[[i]] <- integer(length = ncol(x))
-      for (k in 1L:length(uni.num[[i]])) {
-        ja[[i]][k] <- which(x[i, ] %in% uni.num[[i]][k])
-      }
+      ja[[i]] <- unlist(sapply(uni.num[[i]], 
+                        function(x) which(xmat[i, ] %in% x), simplify = FALSE))
     }
     return(as.integer(unlist(ja)))
   }
-  # Jaslot <- function(x) as.integer(rep(x[nrow(x), ], nrow(x)))
 
   ia <- Iaslot(gmat)
   ja <- Jaslot(gmat)
