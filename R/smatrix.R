732a818ac9688f6752eea08bf602344bc8999d00
Earo Wang <earo.wang@gmail.com>
2013-12-15 23:10:54 +1100

    update jaslot

--- R/smatrix.R
+++ R/smatrix.R
@@ -19,12 +19,23 @@ smatrix <- function(xts) {
   ra <- as.numeric(rep(1, nbts * nrow(gmat)))
 
   # Create Iaslot
-  Iaslot <- function(x) as.integer(cumsum(c(1L, unlist(apply(gmat, 1, table)))))
+  Iaslot <- function(x) as.integer(cumsum(c(1L, unlist(apply(x, 1, table)))))
   # Create Jaslot
-  Jaslot <- function(x) as.integer(rep(gmat[nrow(gmat), ], nrow(gmat)))
+  Jaslot <- function(x) {
+    ja <- vector(length = nrow(x), mode = "list")
+    uni.num <- apply(x, 1, unique)
+    for (i in 1L:length(ja)) {
+      ja[[i]] <- integer(length = ncol(x))
+      for (k in 1L:length(uni.num[[i]])) {
+        ja[[i]][k] <- which(x[i, ] %in% uni.num[[i]][k])
+      }
+    }
+    return(as.integer(unlist(ja)))
+  }
+  # Jaslot <- function(x) as.integer(rep(x[nrow(x), ], nrow(x)))
 
-  ia <- Iaslot(xts)
-  ja <- Jaslot(xts)
+  ia <- Iaslot(gmat)
+  ja <- Jaslot(gmat)
   out <- new("matrix.csr", ra = ra, ja = ja, ia = ia, 
              dimension = as.integer(c(ntotal, nbts)))
   return(out)
