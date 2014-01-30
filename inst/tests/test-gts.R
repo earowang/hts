ccc7fda2973d8c57ac3ed6fc72dbf07a90febe61
EaroWang <earo.wang@gmail.com>
2014-01-30 11:18:52 +1100

    update authors for man files

--- inst/tests/test-gts.R
+++ inst/tests/test-gts.R
@@ -14,7 +14,7 @@ test_that("tests for labels", {
   mts <- ts(5 + matrix(sort(rnorm(2700)), nrow = 100, ncol = 27), 
             start = c(2001, 1), frequency = 12)
   g <- matrix(c(rep(1:3, each = 9), rep(c(rep(1, 3), rep(2, 3), rep(3, 3)), 3),
-              rep(1:3, 9)), nrow = 3, byrow = 9)
+              rep(1:3, 9)), nrow = 3, byrow = TRUE)
   output <- paste0("G", 1:3)
   expect_that(names(gts(mts, g)$labels), equals(output))
 })
@@ -24,7 +24,7 @@ test_that("tests for specified labels", {
   mts <- ts(5 + matrix(sort(rnorm(2700)), nrow = 100, ncol = 27), 
             start = c(2001, 1), frequency = 12)
   g <- matrix(c(rep(1:3, each = 9), rep(c(rep(1, 3), rep(2, 3), rep(3, 3)), 3),
-              rep(1:3, 9)), nrow = 3, byrow = 9)
+              rep(1:3, 9)), nrow = 3, byrow = TRUE)
   rownames(g) <- c("Sex", "Purpose", "Frames")
   expect_that(names(gts(mts, g)$labels), equals(rownames(g)))
 })
@@ -34,7 +34,7 @@ test_that("tests for gmatrix", {
   mts <- ts(5 + matrix(sort(rnorm(2700)), nrow = 100, ncol = 27), 
             start = c(2001, 1), frequency = 12)
   g <- matrix(c(rep(1:3, each = 9), rep(c(rep(1, 3), rep(2, 3), rep(3, 3)), 3),
-              rep(1:3, 9)), nrow = 3, byrow = 9)
+              rep(1:3, 9)), nrow = 3, byrow = TRUE)
   gmat <- rbind(rep(1, 27), g, seq(1, 27))
   class(gmat) <- "gmatrix"
   output <- gts(mts, g)$groups
@@ -48,7 +48,7 @@ test_that("tests for matrix with characters", {
   mts <- ts(5 + matrix(sort(rnorm(1600)), nrow = 100, ncol = 16), 
             start = c(2001, 1), frequency = 12)
   gchar <- matrix(c(rep("Male", 8), rep("Female", 8), rep(LETTERS[3:10], 2)),
-                  nrow = 2, byrow = T)
+                  nrow = 2, byrow = TRUE)
   g <- matrix(c(rep(1, 8), rep(2, 8), rep(1:8, 2)), nrow = 2, byrow = T)
   gmat <- rbind(rep(1, 16), g, seq(1, 16))
   class(gmat) <- "gmatrix"
