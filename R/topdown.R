a11f7119efebb59b4dd7abfe75dea7f11ae4abcf
earowang <earo.wang@gmail.com>
2015-01-10 13:12:06 +1100

    Revert "robust to missing values"
    This reverts commit cd2fba47bf03002cc9733ad5cc29c5d9516c02e4.

--- R/topdown.R
+++ R/topdown.R
@@ -20,13 +20,13 @@ TdGsF <- function(fcasts, bts, topts) {
 
 TdFp <- function(fcasts, nodes) {
   # Top-down forecasts using forecast proportions
-  levels <- cumsum(Mnodes(nodes))
+  levels <- cumsum(hts:::Mnodes(nodes))
   # Split fcasts to a list
   l.levels <- length(levels)
   flist <- lapply(2L:l.levels, function(x) {
                     fcasts[, seq(levels[x - 1L] + 1L, levels[x])]
                   })
-  flist <- c(list(fcasts[, 1L, drop = FALSE]), flist)
+  flist <- c(list(fcasts[, 1L]), flist)
   if (is.vector(flist[[2L]])) {  # In case of h = 1
     new.flist <- vector(length = l.levels - 1L, mode = "list")
     for (j in 1L:(l.levels - 1L)) {
