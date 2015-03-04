c0aaa0db19bf36a12443dc16b72f56990656d3d4
Rob J Hyndman <robjhyndman@gmail.com>
2015-03-04 12:10:21 +1100

    Fixed a bug in accuracy.gts()

--- R/accuracy.gts.R
+++ R/accuracy.gts.R
@@ -19,18 +19,26 @@ accuracy.gts <- function(fcasts, test, levels) {
     stop("Argument test must be a grouped time series.")
   }
 
-  if (missing(test) && !is.null(fcasts$fitted)) { # only returns bottom level
+  if (missing(test))
+  {
+    if(is.null(fcasts$fitted))
+      stop("No fitted values available for historical times, and no actual values available for future times")
+    
     x <- unclass(fcasts$histy)  # Unclass mts to matrix
     res <- x - unclass(fcasts$fitted)  # fcasts$residuals may contain errors
     levels <- ifelse(is.hts(fcasts), length(fcasts$nodes), 
                      nrow(fcasts$groups) - 1L)
-  } else {
+  } 
+  else {
     f <- unclass(aggts(fcasts, levels, forecasts = TRUE))
     x <- unclass(aggts(test, levels))
     res <- x - f
   }
 
-  histy <- aggts(fcasts, levels, forecasts = FALSE)
+  if(is.null(fcasts$histy))
+    histy <- NULL
+  else
+    histy <- aggts(fcasts, levels, forecasts = FALSE)
   if (!is.null(histy)) {
     scale <- colMeans(abs(diff(histy, lag = max(1, frequency(histy)))), 
                       na.rm = TRUE)
