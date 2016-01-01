52fd61f16ff5d6ba01c0ec07f3bb9dbab2679db0
ShanikaLW <shaniw_21@yahoo.com>
2016-01-01 23:44:11 +1100

    Fixed a bug in handling NA

--- R/combineg.R
+++ R/combineg.R
@@ -1,6 +1,6 @@
 SLM <- function(fcasts, S, weights = NULL) {
   class(fcasts) <- tsp(fcasts) <- NULL
-  fcasts <- na.omit(fcasts) # In case of "NA"
+  fcasts <- t(na.omit(t(fcasts))) # In case of "NA"
   if (is.null(weights)) {
     coef <- slm.fit(S, fcasts)$coefficients
   } else {
