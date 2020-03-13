e1dded98eacf531cb2627b91b635de6ae1174f13
ShanikaLW <shanika.wickramasuriya@gmail.com>
2020-03-13 18:05:04 +1300

    Update combinef.R
    Allow computation of non-negative reconciled forecasts.

--- R/combinef.R
+++ R/combinef.R
@@ -59,9 +59,10 @@
 #' plot(y.f)}
 #' 
 #' @export combinef
-combinef <- function(fcasts, nodes, groups, weights = NULL,
+combinef <- function(fcasts, nodes, groups, weights = NULL, nonnegative = FALSE, 
+                     parallel = FALSE, num.cores = 2,
                      algorithms = c("lu", "cg", "chol", "recursive", "slm"),
-                     keep = c("gts", "all", "bottom")) {
+                     keep = c("gts", "all", "bottom"), ...) {
   # Construct optimal combination forecasts
   #
   # Args:
@@ -69,38 +70,106 @@ combinef <- function(fcasts, nodes, groups, weights = NULL,
   #   nodes: nodes for hts
   #   groups: gts
   #   weights: users need to specify the weights
+  #   nonnegative: non-negativity of the reconciled forecasts
+  #   parallel: import parallel package to allow parallel processing
+  #   num.cores: specify how many cores are going to be used
   #   algorithms: different algorithms to obtain reconciled forecasts
   #   keep: choose to return a gts object/all ts/bottom time series
+  #   ...: other arguments to be passed to non-negative algorithm
   #
   # Return:
-  #   Optimal forecasts
+  #   Optimal (non-negative) reconciled forecasts
   alg <- match.arg(algorithms)
   keep <- match.arg(keep)
   fcasts <- stats::as.ts(fcasts)
   tspx <- stats::tsp(fcasts)
   cnames <- colnames(fcasts)
-  if (missing(groups)) { # hts class
-    if (alg == "slm") {
-      stop("The slm algorithm does not support an hts object.", call. = FALSE)
-    }
-    totalts <- sum(Mnodes(nodes))
-    if (!is.matrix(fcasts)) {
-      fcasts <- t(fcasts)
-    }
-    h <- nrow(fcasts)
-    if (ncol(fcasts) != totalts) {
-      stop("Argument fcasts requires all the forecasts.", call. = FALSE)
-    }
-    if (alg == "recursive") { # only nodes to be needed
-      # CombineH only returns bottom time series
-      if(is.null(weights)) {
-        bf <- CombineH(fcasts, nodes)  # w/o weights
-      } else {
-        bf <- CombineHw(fcasts, nodes, weights)  # with weights
+  
+  if (!nonnegative) {
+    if (missing(groups)) { # hts class
+      if (alg == "slm") {
+        stop("The slm algorithm does not support an hts object.", call. = FALSE)
       }
-    } else {
-      # Other algorithms return all time series
-      gmat <- GmatrixH(nodes)
+      totalts <- sum(Mnodes(nodes))
+      if (!is.matrix(fcasts)) {
+        fcasts <- t(fcasts)
+      }
+      h <- nrow(fcasts)
+      if (ncol(fcasts) != totalts) {
+        stop("Argument fcasts requires all the forecasts.", call. = FALSE)
+      }
+      if (alg == "recursive") { # only nodes to be needed
+        # CombineH only returns bottom time series
+        if(is.null(weights)) {
+          bf <- CombineH(fcasts, nodes)  # w/o weights
+        } else {
+          bf <- CombineHw(fcasts, nodes, weights)  # with weights
+        }
+      } else {
+        # Other algorithms return all time series
+        gmat <- GmatrixH(nodes)
+        fcasts <- t(fcasts)
+        if (alg == "chol") {
+          smat <- Smatrix(gmat)
+          if (!is.null(weights)) {
+            weights <-  methods::as(1/weights, "matrix.diag.csr")
+          }
+          allf <- CHOL(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
+        } else {
+          smat <- SmatrixM(gmat)
+          if (!is.null(weights)) {
+            seqts <- 1:totalts
+            weights <- sparseMatrix(i = seqts, j = seqts, x = 1/weights)
+          }
+          if (alg == "lu") {
+            allf <- LU(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
+          } else if (alg == "cg") {
+            allf <- CG(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
+          }
+        }
+      }
+      
+      if (keep == "all") {
+        if (alg == "recursive") {
+          gmat <- GmatrixH(nodes)
+          levels <- 1L:nrow(gmat)
+          # A function to aggregate the bts
+          if (h == 1 && !is.null(weights)) {
+            rSum <- function(x) rowsum(as.matrix(bf), gmat[x, ], reorder = FALSE,
+                                       na.rm = TRUE)
+          } else {
+            rSum <- function(x) rowsum(t(bf), gmat[x, ], reorder = FALSE,
+                                       na.rm = TRUE)
+          }
+          ally <- lapply(levels, rSum)
+          # Convert lists to matrices
+          out <- matrix(unlist(sapply(ally, t)), nrow = h)
+        } else {
+          out <- t(allf)
+        }
+      } else {
+        if (alg != "recursive") {
+          bottom <- totalts - (ncol(smat):1L) + 1L
+          bf <- t(allf[bottom, ])
+        }
+        if (keep == "gts") {
+          bf <- ts(bf, start = tspx[1L], frequency = tspx[3L])
+          out <- suppressMessages(hts(bf, nodes = nodes))
+        } else {
+          out <- bf
+        }
+      }
+    } else if (missing(nodes)) {  # gts class
+      if (alg == "recursive") {
+        stop("The recursive algorithm does not support a gts object.", call. = FALSE)
+      }
+      # To call Smatrix() properly
+      rownames(groups) <- NULL
+      gmat <- GmatrixG(groups)
+      totalts <- sum(Mlevel(gmat))
+      if (ncol(fcasts) != totalts) {
+        stop("Argument fcasts requires all the forecasts.", call. = FALSE)
+      }
       fcasts <- t(fcasts)
       if (alg == "chol") {
         smat <- Smatrix(gmat)
@@ -108,6 +177,9 @@ combinef <- function(fcasts, nodes, groups, weights = NULL,
           weights <-  methods::as(1/weights, "matrix.diag.csr")
         }
         allf <- CHOL(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
+      } else if (alg == "slm") {
+        smat <- Smatrix(gmat)
+        allf <- SLM(fcasts = fcasts, S = smat, weights = weights)
       } else {
         smat <- SmatrixM(gmat)
         if (!is.null(weights)) {
@@ -120,83 +192,55 @@ combinef <- function(fcasts, nodes, groups, weights = NULL,
           allf <- CG(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
         }
       }
-    }
-
-    if (keep == "all") {
-      if (alg == "recursive") {
-        gmat <- GmatrixH(nodes)
-        levels <- 1L:nrow(gmat)
-        # A function to aggregate the bts
-        if (h == 1 && !is.null(weights)) {
-          rSum <- function(x) rowsum(as.matrix(bf), gmat[x, ], reorder = FALSE,
-                                     na.rm = TRUE)
-        } else {
-          rSum <- function(x) rowsum(t(bf), gmat[x, ], reorder = FALSE,
-                                     na.rm = TRUE)
-        }
-        ally <- lapply(levels, rSum)
-        # Convert lists to matrices
-        out <- matrix(unlist(sapply(ally, t)), nrow = h)
-      } else {
+      
+      if (keep == "all") {
         out <- t(allf)
-      }
-    } else {
-      if (alg != "recursive") {
+      } else {
         bottom <- totalts - (ncol(smat):1L) + 1L
         bf <- t(allf[bottom, ])
-      }
-      if (keep == "gts") {
-        bf <- ts(bf, start = tspx[1L], frequency = tspx[3L])
-        out <- suppressMessages(hts(bf, nodes = nodes))
-      } else {
-        out <- bf
+        if (keep == "gts") {
+          colnames(bf) <- cnames[bottom]
+          bf <- ts(bf, start = tspx[1L], frequency = tspx[3L])
+          out <- suppressMessages(gts(bf, groups = groups))
+        } else {
+          out <- bf
+        }
       }
     }
-  } else if (missing(nodes)) {  # gts class
-    if (alg == "recursive") {
-      stop("The recursive algorithm does not support a gts object.", call. = FALSE)
-    }
-    # To call Smatrix() properly
-    rownames(groups) <- NULL
-    gmat <- GmatrixG(groups)
-    totalts <- sum(Mlevel(gmat))
-    if (ncol(fcasts) != totalts) {
-      stop("Argument fcasts requires all the forecasts.", call. = FALSE)
-    }
-    fcasts <- t(fcasts)
-    if (alg == "chol") {
-      smat <- Smatrix(gmat)
-      if (!is.null(weights)) {
-        weights <-  methods::as(1/weights, "matrix.diag.csr")
-      }
-      allf <- CHOL(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
-    } else if (alg == "slm") {
-      smat <- Smatrix(gmat)
-      allf <- SLM(fcasts = fcasts, S = smat, weights = weights)
+  } else {
+    if (alg %in% c("recursive", "slm")) {
+      stop("The non-negative algorithm doesn't support slm or recursive", call. = FALSE)
     } else {
-      smat <- SmatrixM(gmat)
-      if (!is.null(weights)) {
-        seqts <- 1:totalts
-        weights <- sparseMatrix(i = seqts, j = seqts, x = 1/weights)
-      }
-      if (alg == "lu") {
-        allf <- LU(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
-      } else if (alg == "cg") {
-        allf <- CG(fcasts = fcasts, S = smat, weights = weights, allow.changes = FALSE)
+      if (parallel) {
+        if (is.null(num.cores)) {
+          num.cores <- detectCores()
+        }
+        cl <- makeCluster(num.cores)
+        bf <- parSapplyLB(cl = cl, X = fcasts, bpv, nodes, groups, weights = weights, alg = alg, ..., simplify = TRUE)
+        stopCluster(cl = cl)
+      } else {
+        bf <- sapply(fcasts, bpv, nodes, groups, weights = weights, alg = alg, ...)
       }
     }
-
-    if (keep == "all") {
-      out <- t(allf)
+    bf <- ts(bf, start = tspx[1L], frequency = tspx[3L])
+  }
+  
+  if (missing(groups)) {
+    if (keep == "bottom") {
+      out <- bf
     } else {
-      bottom <- totalts - (ncol(smat):1L) + 1L
-      bf <- t(allf[bottom, ])
-      if (keep == "gts") {
-        colnames(bf) <- cnames[bottom]
-        bf <- ts(bf, start = tspx[1L], frequency = tspx[3L])
-        out <- suppressMessages(gts(bf, groups = groups))
-      } else {
-        out <- bf
+      out <- suppressMessages(hts(bf, nodes = nodes))
+      if (keep == "all") {
+        out <- aggts(out)
+      }
+    }
+  } else {
+    if (keep == "bottom") {
+      out <- bf
+    } else {
+      out <- suppressMessages(gts(bf, groups = groups))
+      if (keep == "all") {
+        out <- aggts(out)
       }
     }
   }
