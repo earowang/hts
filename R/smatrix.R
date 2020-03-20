211f1521c82a9ff69af285e7de474aec44363804
ShanikaLW <shanika.wickramasuriya@gmail.com>
2020-03-20 16:44:50 +1300

    Update smatrix.R
    Reference style is updated.

--- R/smatrix.R
+++ R/smatrix.R
@@ -9,8 +9,8 @@
 #' @author Earo Wang
 #' @seealso \code{\link[hts]{hts}}, \code{\link[hts]{gts}},
 #' \code{\link[hts]{combinef}}
-#' @references R. J. Hyndman, R. A. Ahmed, G. Athanasopoulos and H.L. Shang
-#' (2011) Optimal combination forecasts for hierarchical time series.
+#' @references Hyndman, R. J., Ahmed, R. A., Athanasopoulos, G., & Shang, H. L.
+#' (2011). Optimal combination forecasts for hierarchical time series.
 #' \emph{Computational Statistics and Data Analysis}, \bold{55}(9), 2579--2589.
 #' \url{http://robjhyndman.com/papers/hierarchical/}
 #' @keywords ts
